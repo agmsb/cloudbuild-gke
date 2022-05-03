1b175618786de9bc8272e550fc8a8d2a935b01a7
Anthony Bushong <agmsb@google.com>
2022-05-02 21:15:46 -0700

    Update need for VPN.

--- secure-software-delivery/infra/create_all.sh
+++ secure-software-delivery/infra/create_all.sh
@@ -1,5 +1,6 @@
 source ./secrets/create_secret.sh
 source ./iam/create_iam.sh
 source ./vpc/create_vpc.sh
+source ./vpc/vpn/create_vpn.sh
 source ./private_pool/create_private_pool.sh
 source ./cluster/create_cluster.sh
--- secure-software-delivery/infra/delete_all.sh
+++ secure-software-delivery/infra/delete_all.sh
@@ -1,5 +1,6 @@
 source ./cluster/delete_cluster.sh
 source ./private_pool/delete_private_pool.sh
+source ./vpc/vpn/delete_vpn.sh
 source ./vpc/delete_vpc.sh
-source ./secrets/delete_secret.sh
 source ./iam/delete_iam.sh
+source ./secrets/delete_secret.sh
