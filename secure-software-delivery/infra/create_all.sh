99f9817c7b278dfa63a101b93e5982b80c107faf
Anthony Bushong <agmsb@google.com>
2022-05-03 14:58:22 -0700

    Fix ordering of create all.

--- secure-software-delivery/infra/create_all.sh
+++ secure-software-delivery/infra/create_all.sh
@@ -3,5 +3,5 @@ source ./secrets/create_secret.sh
 source ./iam/create_iam.sh
 source ./vpc/create_vpc.sh
 source ./vpc/vpn/create_vpn.sh
-source ./private_pool/create_private_pool.sh
 source ./cluster/create_cluster.sh
+source ./private_pool/create_private_pool.sh
