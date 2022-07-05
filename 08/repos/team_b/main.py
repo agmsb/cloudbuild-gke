9d693c2d4563e3c454bdc87f2124aa725487e451
Anthony Bushong <agmsb@google.com>
2022-07-05 04:28:11 +0000

    catch up team b python app

--- 08/repos/team_b/main.py
+++ 08/repos/team_b/main.py
@@ -19,14 +19,19 @@ products = ['BigQuery',
 @app.route('/')
 def hello_product():
     product = random.choice(products)
-    message = """
-Which Google Cloud product are you? \n
-Your Google Cloud product is: {product}! \n \n
-Built by: {version} \n
-You've been served by Pod: {pod} in Namespace: {namespace} on Node: {node} \n
-""".format(version=version, product=product, pod=pod, namespace=namespace, node=node)
 
-    return(message)
+    message = (
+        "Your Google Cloud product is: {product}! \n\n\n"
+        "Built by: {version} \n"
+        "You've been served by...\n"
+        "Pod: {pod}\n"
+        "Running in Namespace: {namespace}\n"
+        "Scheduled to Node: {node}\n"
+    ).format(version=version, product=product, pod=pod, namespace=namespace, node=node)
+    
+    response = make_response(message)
+    response.headers['content-type'] = 'text/plain'
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
